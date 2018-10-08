d23f3cf989e9e5af4465cd6fda88fdc64481221d
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-10-08 16:44:35 -0400

    create and configure Google Maps Component

---
+++ src/app/google-maps/google-maps.component.ts
@@ -0,0 +1,22 @@
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-google-maps',
+  templateUrl: './google-maps.component.html',
+  styleUrls: ['./google-maps.component.css']
+})
+export class GoogleMapsComponent implements OnInit {
+
+  constructor() { }
+
+ 
+  
+  ngOnInit() {
+    
+  }
+  
+  
+
+}
