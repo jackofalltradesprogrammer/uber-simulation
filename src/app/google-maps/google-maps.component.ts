9d17eeca415bd271cb8e357f090d6293fb29a233
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-10-08 16:49:03 -0400

    Create a simple map with Manhattan as center

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -1,6 +1,6 @@
-
+/// <reference types="@types/googlemaps" />
 import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { Location } from '../google-maps/location';
 
 @Component({
   selector: 'app-google-maps',
@@ -10,11 +10,30 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class GoogleMapsComponent implements OnInit {
 
   constructor() { }
-
+  // gmapElement is a reference to <div #gmap> inside html file
+  @ViewChild('gmap')
+  gmapElement: any;
  
-  
-  ngOnInit() {
+  map: google.maps.Map;
+
+  // center the map to this lat and long
+  manhattan: Location = {
+    latitude: 40.7831,
+    longitude: -73.9712
+  };
+  centerMap: any = new google.maps.LatLng(this.manhattan.latitude, this.manhattan.longitude);
     
+  ngOnInit() {
+
+     // Define map properties
+     var mapProp = {
+      center: this.centerMap,
+      zoom: 15,
+      // styles: this.customMap,
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
   }
   
   
